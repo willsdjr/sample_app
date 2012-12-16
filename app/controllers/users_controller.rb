2ce4810a81837da338afcf4f11ed4f07a3d90d9c
dennis wills <dlwills@madisoncollege.edu>
2012-12-15 18:58:21 -0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
     before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
     before_filter :correct_user,   only: [:edit, :update]
+    before_filter :admin_user,     only: :destroy
     
   def show
       @user = User.find(params[:id])
-  end
+      @microposts = @user.microposts.paginate(page: params[:page])
+    end
+  
   
   def new
       @user = User.new
@@ -48,15 +51,14 @@ class UsersController < ApplicationController
    
     private
 
-    def signed_in_user
-       unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+   
     
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,52 +49,76 @@ describe "Authentication" do
   describe "for non-signed-in users" do
    let(:user) { FactoryGirl.create(:user) }
 
-    describe "when attempting to visit a protected page" do
-        before do
+     describe "when attempting to visit a protected page" do
+          before do
           visit edit_user_path(user)
           fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
           click_button "Sign in"
-        end
+          end
 
         describe "after signing in" do
-
-          it "should render the desired protected page" do
+            it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
-          end
+            end
         end
-      end
-     describe "in the Users controller" do
+     describe "in the Microposts controller" do
 
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
         end
 
-        describe "submitting to the update action" do
-          before { put user_path(user) }
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { response.should redirect_to(signin_path) }
         end
       end
-       describe "visiting the user index" do
+    end
+     describe "in the Users controller" do
+
+          describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+          end
+
+          describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+          end
+      end
+          describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
-        end
+          end
+          
     end
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user }
 
-      describe "visiting Users#edit page" do
+        describe "visiting Users#edit page" do
         before { visit edit_user_path(wrong_user) }
         it { should_not have_selector('title', text: full_title('Edit user')) }
-      end
+        end
 
-      describe "submitting a PUT request to the Users#update action" do
+        describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }
-      end
+        end
+     end
+    
+    describe "as non-admin user" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:non_admin) { FactoryGirl.create(:user) }
+
+       before { sign_in non_admin }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
+        end
     end
   end
 end
